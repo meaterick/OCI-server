f8ab7c36d0241b6f8f17a69fba12f4447b090efc
meaterick <89893748+meaterick@users.noreply.github.com>
2024-09-02 12:40:14 +0900

    Update index.js

--- index.js
+++ index.js
@@ -3,8 +3,29 @@ const express = require('express')
 , path = require('path')
 , static = require('serve-static');
 
+const { MongoClient } = require("mongodb");
 const app = express()
 const port = 80
+
+const url = "mongodb+srv://meaterick:qwe123VVBPLK09meate@firstdb.nye4r.mongodb.net/?retryWrites=true&w=majority&appName=firstDB";
+const client = new MongoClient(url);
+
+async function run() {
+  try {
+    const database = client.db('sample_mflix');
+    const movies = database.collection('movies');
+
+    // Query for a movie that has the title 'Back to the Future'
+    const query = { title: 'Back to the Future' };
+    const movie = await movies.findOne(query);
+
+    console.log(movie);
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('src'));

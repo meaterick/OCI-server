5513248e310fb92b93adb518f761f4e8e0a4636b
meaterick <89893748+meaterick@users.noreply.github.com>
2024-09-09 09:58:27 +0900

    Update index.js

--- index.js
+++ index.js
@@ -65,7 +65,7 @@ run().catch(console.dir);
 const loginLimiter = rateLimit({
     windowMs: 1 * 60 * 1000, // 15 minutes
     max: 5, // Limit each IP to 5 login requests per 15 minutes
-    message: "Too many login attempts, please try again after 15 minutes.",
+    message: "Too many login attempts, please try again after 1 minutes.",
     standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
     legacyHeaders: false, // Disable the `X-RateLimit-*` headers
 });
